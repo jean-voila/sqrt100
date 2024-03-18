b0a14604f9762adde18f84c12e11041729a8f6cb
Jean <jean@beaudelair.local>
2024-03-18 16:37:18 +0100

    debut du multiplayer, tests de connections serveur/client

--- Castle Of Demise/Scripts/MultiplayerMenu.cs
+++ Castle Of Demise/Scripts/MultiplayerMenu.cs
@@ -1,8 +1,15 @@
 using Godot;
 using System;
 
+
 public class MultiplayerMenu : Control
 {
+
+
+    private NetworkedMultiplayerENet clientPeer = new NetworkedMultiplayerENet();
+    private string serverIP = "";
+    
+    
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -13,13 +20,63 @@ public class MultiplayerMenu : Control
         
     }
 
+    private void _hostPressed()
+    {
+        var serverPeer = new NetworkedMultiplayerENet();
+        serverPeer.CreateServer(8910, 2);
+        GetTree().NetworkPeer = serverPeer;
+
+        
+
+
+    }
+
+    private void _clientPressed()
+    {
+        GetNode<Node2D>("%JoinOptions").Visible = !GetNode<Node2D>("%JoinOptions").Visible;
+        
+
+    }
+
+    // Bouton appuyé après avoir entré l'adresse IP du serveur
+    private void _serverConnectPressed()
+    {
+        
+        
+        clientPeer.CreateClient(serverIP, 8910);
+        GetTree().NetworkPeer = clientPeer;
+
+        
+
+
+
+    }
+
+    private void _server_IP_Input(string ip)
+    {
+        serverIP = ip;
+    }
+    
+
     private void _back()
     {
         GetTree().ChangeScene("res://menus/GamemodeMenu.tscn");
     }
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Process(float delta)
+    {
+      
+        if (clientPeer.GetConnectionStatus() == NetworkedMultiplayerPeer.ConnectionStatus.Connecting)
+        {
+            GetNode<RichTextLabel>("%ServerIsConnectedDebug").Text = "c en train d'être bon frerot";
+        } else if (clientPeer.GetConnectionStatus() == NetworkedMultiplayerPeer.ConnectionStatus.Connected)
+        {
+            GetNode<RichTextLabel>("%ServerIsConnectedDebug").Text = "c bon frerot";
+
+        }
+        else
+        {
+            GetNode<RichTextLabel>("%ServerIsConnectedDebug").Text = "c  pas bon frerot";
+        }
+    }
 }
