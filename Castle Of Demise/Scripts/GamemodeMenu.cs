f4515d78f7e1eea83a19875d01b6fe403c2000e2
Paul <placanette@gmail.com>
2024-03-21 11:01:39 +0100

    update

--- Castle Of Demise/Scripts/GamemodeMenu.cs
+++ Castle Of Demise/Scripts/GamemodeMenu.cs
@@ -3,28 +3,28 @@ using System;
 
 public class GamemodeMenu : Control
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
 
 
 
-    private void _singlePlayer()
-    {
-        GetTree().ChangeScene("res://maps/mpMap01.tscn");
-    }
+	private void _singlePlayer()
+	{
+		GetTree().ChangeScene("res://maps/mpMap01.tscn");
+	}
 
-    private void _multiplayerPressed()
-    {
-        GetNode<Control>("%GamemodeMenu").Hide();
-        GetNode<Control>("%MultiplayerMenu").Show();
-    }
+	private void _multiplayerPressed()
+	{
+		GetNode<Control>("%GamemodeMenu").Hide();
+		GetNode<Control>("%MultiplayerMenu").Show();
+	}
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
--- Castle Of Demise/Scripts/TitleScreen.cs
+++ Castle Of Demise/Scripts/TitleScreen.cs
@@ -3,59 +3,65 @@ using System;
 
 public class TitleScreen : Control
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
-    
-    private void _PlayButtonPressed()
-    {
-        GetNode<Control>("%TitleMenu").Hide();
-        GetNode<Control>("%MultiplayerMenu").Hide();
-        GetNode<Control>("%GamemodeMenu").Show();
-        
-        
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+	
+	private void _PlayButtonPressed()
+	{
+		GetNode<Control>("%TitleMenu").Hide();
+		GetNode<Control>("%MultiplayerMenu").Hide();
+		GetNode<Control>("%GamemodeMenu").Show();
+		
+		
+	}
 
-    private void _Exit()
-    {
-        GetTree().Quit();
-    }
+	private void _Exit()
+	{
+		GetTree().Quit();
+	}
 
-    private void _OptionButtonPressed()
-    {
-        GetNode<Control>("%TitleMenu").Hide();
-        GetNode<Control>("%OptionsMenu").Show();
-    }
+	private void _OptionButtonPressed()
+	{
+		GetNode<Control>("%TitleMenu").Hide();
+		GetNode<Control>("%OptionsMenu").Show();
+	}
 
-    private void _focused_button()
-    {
-        GetNode<AudioStreamPlayer2D>("%FocusedButtonSound").Play();
-    }
+	private void _focused_button()
+	{
+		GetNode<AudioStreamPlayer2D>("%FocusedButtonSound").Play();
+	}
 
-    private void _pressed_button()
-    {
-        GetNode<AudioStreamPlayer2D>("%PressedButtonSound").Play();
-    }
+	private void _pressed_button()
+	{
+		GetNode<AudioStreamPlayer2D>("%PressedButtonSound").Play();
+	}
 
-    private void _show_title_screen_()
-    {
-        GetNode<Control>("%GamemodeMenu").Hide();
-        GetNode<Control>("%OptionsMenu").Hide();
-        GetNode<Control>("%TitleMenu").Show();
-    }
-    
+	private void _show_title_screen_()
+	{
+		GetNode<Control>("%GamemodeMenu").Hide();
+		GetNode<Control>("%OptionsMenu").Hide();
+		GetNode<Control>("%TitleMenu").Show();
+	}
+	
+	private void _DebugButtonPressed()
+{
+	GetTree().ChangeScene("res://maps/TestsMap.tscn");
+}
+
+	
+
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-      if (Input.IsActionPressed("key_f6"))
-      {
-          GetTree().ChangeScene("res://maps/DebugMap.tscn");
-      }
+
   }
 }
+
+
