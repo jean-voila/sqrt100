80d543f3ffddbbe00119f8fe74b372106bf8897a
Jean <47366872+jean-voila@users.noreply.github.com>
2024-03-21 16:21:46 +0100

    correction d'un s en trop

--- Castle Of Demise/mobs/Player/Steps.cs
+++ Castle Of Demise/mobs/Player/Steps.cs
@@ -12,14 +12,28 @@ public partial class Player
             _stepTimer = GetNode<Timer>("StepSoundsEffetcs/TimerBetweenStep");
             _floorRayCast = GetNode<RayCast>("checkFloor");
         }
+        
         private void HandleStepSounds(bool isMoving)
         {
-            if (isMoving && _SEEnabled && _floorRayCast.GetCollider() != null && _stepTimer.TimeLeft <= 0)
+            var onTheGround  = _floorRayCast.GetCollider() != null;
+            var canPlayNextStep = _stepTimer.TimeLeft <= 0;
+            if (isMoving && _SEEnabled && onTheGround && canPlayNextStep)
             {
+                
+                // Select a random step sound from a list of different step sounds
                 var randomStep = new Random().Next(0, _stepSounds.Count);
+
+                // Set the pitch of the sound to a random value between 1 and 5
                 _stepSounds[randomStep].PitchScale = new Random().Next(1, 5);
+
+                // Play the selected step sound
                 _stepSounds[randomStep].Play();
+
+                // Restart the timer
                 _stepTimer.Start(0.2f);
             }
         }
+
+
+
     }
