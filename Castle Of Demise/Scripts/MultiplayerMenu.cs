73d3a55cb8a3fbd4ab943d7748969f7f553d3cf0
Medèu <47332436+afendyr@users.noreply.github.com>
2024-04-20 23:34:39 +0200

    multiplayer test

--- Castle Of Demise/Scripts/MultiplayerMenu.cs
+++ Castle Of Demise/Scripts/MultiplayerMenu.cs
@@ -6,46 +6,65 @@ public class MultiplayerMenu : Control
 {
 
 
-    private NetworkedMultiplayerENet clientPeer;
-    private NetworkedMultiplayerENet serverPeer;
-    private string serverIP = "";
+    private NetworkedMultiplayerENet _clientPeer;
+    private NetworkedMultiplayerENet _serverPeer;
+    private string _serverIp = "";
     
     
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+    
 
     private void _hostPressed()
     {
-        serverPeer = new NetworkedMultiplayerENet();
-        serverPeer.CreateServer(8910, 2);
-        GetTree().NetworkPeer = serverPeer;
+        _serverPeer = new NetworkedMultiplayerENet();
+        _serverPeer.CreateServer(8910, 2);
+        GetTree().NetworkPeer = _serverPeer;
         
     }
 
     private void _clientPressed()
     {
+        _clientPeer = new NetworkedMultiplayerENet();
+        _clientPeer.CreateClient(_serverIp, 8910);
+        GetTree().NetworkPeer = _clientPeer;
         GetNode<Node2D>("%JoinOptions").Visible = !GetNode<Node2D>("%JoinOptions").Visible;
-        
     }
 
+    /*
+    
     // Bouton appuyé après avoir entré l'adresse IP du serveur
+    // Ancien bouton, maintenant unitilisé à compter du 20/04/2024
     private void _serverConnectPressed()
     {
-        clientPeer = new NetworkedMultiplayerENet();
-        clientPeer.CreateClient(serverIP, 8910);
-        GetTree().NetworkPeer = clientPeer;
+        _clientPeer = new NetworkedMultiplayerENet();
+        _clientPeer.CreateClient(_serverIp, 8910);
+        GetTree().NetworkPeer = _clientPeer;
     }
 
+*/
+    public override void _Ready()
+    {
+        GetTree().Connect("network_peer_connected", this, nameof(_PlayerConnected));
+        GetTree().Connect("network_peer_disconnected", this, nameof(_PlayerDisconnected));
+    }
+
+
+    private void _PlayerConnected(int id)
+    {
+        GD.Print("Player with ID: " + id + " has connected.");
+        GetTree().ChangeScene("res://maps/mpMap01.tscn");
+    }
+
+    private void _PlayerDisconnected(int id)
+    {
+    GD.Print("Player with ID: " + id + " has disconnected.");
+    }
     private void _server_IP_Input(string ip)
     {
-        serverIP = ip;
+        _serverIp = ip;
     }
     
 
@@ -54,13 +73,13 @@ public class MultiplayerMenu : Control
     public override void _Process(float delta)
     {
 
-        if (clientPeer != null)
+        if (_clientPeer != null)
         {
-            GetNode<RichTextLabel>("%ServerIsConnectedDebug").Text = clientPeer.GetConnectionStatus().ToString();
+            GetNode<RichTextLabel>("%ServerIsConnectedDebug").Text = _clientPeer.GetConnectionStatus().ToString();
         }
-        if (serverPeer!=null)
+        if (_serverPeer!=null)
         {
-            GetNode<RichTextLabel>("%ClientConnected").Text = serverPeer.GetConnectionStatus().ToString();
+            GetNode<RichTextLabel>("%ClientConnected").Text = _serverPeer.GetConnectionStatus().ToString();
         }
     }
 }
