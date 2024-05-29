d2f7a3a802090b720fc763ba128385830f678754
Medèu <amadeo.heaulme@epita.fr>
2024-05-29 20:28:33 +0200

    connexion multi effectuée

--- Castle Of Demise/Scripts/Menus/MultiLauncher/SetupGameAsHost.cs
+++ Castle Of Demise/Scripts/Menus/MultiLauncher/SetupGameAsHost.cs
@@ -13,13 +13,6 @@ public partial class SetupGameAsHost : Control
 	private int? _scoreToReachValue = null;
 	private int? _gameModeValue = null;
 	
-	
-	
-	public ENetMultiplayerPeer Peer = new();
-	public const int DefaultPort = 8910;
-	public const int MaxNumberOfPeers = 2;
-	public const string ServerIp = "127.0.0.1";
-	
 	public override void _Ready()
 	{
 		_chooseGameMode = GetNode<ItemList>("ChooseGameMode");
@@ -31,12 +24,6 @@ public partial class SetupGameAsHost : Control
 		
 	}
 	
-	
-	private void PlayerConnected(int id)
-	{
-		//_add_player(id);
-		GD.Print("Player connected: " + id);
-	}
 
 	// Ces 2 fonctions sont des fonctions de select pour le multi
 	private void _on_choose_game_mode_item_selected(int index)
@@ -86,46 +73,11 @@ public partial class SetupGameAsHost : Control
 	
 	private void _on_start_game_pressed()
 	{
-		 var err = Peer.CreateServer(DefaultPort, MaxNumberOfPeers);
-         		if (err != Error.Ok)
-         		{
-         			// Is another server running?
-         			GD.Print("Cannot host server");
-         			return;
-         		}
-         
-         		var multiplayerApi = GetTree().GetMultiplayer();
-         		multiplayerApi.MultiplayerPeer = Peer;
-         		GetTree().SetMultiplayer(multiplayerApi);
-         		GD.Print("Waiting for players to connect...");
+		_startGameButton.Disabled = true;
 	}
 
 
 }
 
 
-/*
-private void _hostPressed()  
-        {
-            _peer = new ENetMultiplayerPeer();
-            
-             * _peer.CompressionMode = ENetMultiplayerPeer.CompressionModeEnum.RangeCoder;
-             
-Error err = _peer.CreateServer(DefaultPort, MaxNumberOfPeers);
-if (err != Error.Ok)
-{
-	// Is another server running?
-	return;
-}
-GetTree().ChangeSceneToFile("res://maps/mpMap01.tscn");
-_add_player();
-}
 
-private void _add_player(int id = 1)
-{
-	var player = GD.Load<PackedScene>("res://maps/mpMap01.tscn").Instantiate();
-	player.Name = id.ToString();
-	/* GetTree().Root.AddChild(player);
-	
-}
-*/
--- Castle Of Demise/Scripts/Menus/MultiplayerMenu.cs
+++ Castle Of Demise/Scripts/Menus/MultiplayerMenu.cs
@@ -2,35 +2,82 @@ using Godot;
 
 namespace CastleOfDemise.Scripts.Menus
 {
-    
+
 
     public partial class MultiplayerMenu : Control
     {
 
-    
-    
-    
-        
+        [Export] private int port = 8910;
+        [Export] private string address = "127.0.0.1";
 
+        private ENetMultiplayerPeer peer;
 
+// Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            Multiplayer.PeerConnected += PeerConnected;
+            Multiplayer.PeerDisconnected += PeerDisconnected;
+            Multiplayer.ConnectedToServer += ConnectedToServer;
+            Multiplayer.ConnectionFailed += ConnectionFailed;
+        }
+
+        private void ConnectionFailed()
+        {
+            GD.Print("CONNECTION FAILED");
+        }
+
+        private void ConnectedToServer()
+        {
+            GD.Print("CONNECTED TO SERVER");
+        }
+
+        private void PeerDisconnected(long id)
+        {
+            GD.Print("PLAYER DISCONNECTED: " + id.ToString());
+        }
+
+        private void PeerConnected(long id)
+        {
+            GD.Print("PLAYER CONNECTED! " + id.ToString());
+        }
+
+// Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+        }
         private void _clientPressed()
         {
-            GetNode<Control>("%MultiplayerMenu").Hide();
-            // GetNode<Control>("%GamemodeMenu").Hide();
-            GetNode<Control>("%SetupGameAsClient").Show();
+            peer = new ENetMultiplayerPeer();
+            peer.CreateClient(address, port);
+            peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+            Multiplayer.MultiplayerPeer = peer;
+            GD.Print("JOINING GAME...");
         }
-        
+
         private void _hostPressed()
         {
             GetNode<Control>("%MultiplayerMenu").Hide();
             // GetNode<Control>("%GamemodeMenu").Hide();
             GetNode<Control>("%SetupGameAsHost").Show();
+
+
+            peer = new ENetMultiplayerPeer();
+            var error = peer.CreateServer(port, 2);
+            if (error != Error.Ok)
+            {
+                GD.Print("ERROR CANNOT HOST: " + error.ToString());
+                return;
+            }
+
+            peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+            Multiplayer.MultiplayerPeer = peer;
+            GD.Print("WAITING FOR PLAYERS!");
         }
-        
-        // test
-       
-    
-       
-      
     }
+
+    // test
+
+
+
+
 }
