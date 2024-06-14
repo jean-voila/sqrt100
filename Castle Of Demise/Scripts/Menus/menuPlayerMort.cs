cddba9ca31ac195ae707fc5504a2aebb805a7be5
Kotto654 <placanette@gmail.com>
2024-06-14 10:08:51 +0200

    death screen

---
+++ Castle Of Demise/Scripts/Menus/menuPlayerMort.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class menuPlayerMort : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_button_button_up()
+	{
+		GetTree().ChangeSceneToFile("res://menus/TitleScreen.tscn");
+	}
+}
