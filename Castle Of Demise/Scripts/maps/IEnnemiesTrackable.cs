39b877ed46cba4e10d46ebad1874533d57eef868
Jean <47366872+jean-voila@users.noreply.github.com>
2024-06-15 18:33:41 +0200

    Avancées Leidenstadt

--- Castle Of Demise/Scripts/maps/IEnnemiesTrackable.cs
+++ Castle Of Demise/Scripts/maps/IEnnemiesTrackable.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Linq;
 using CastleOfDemise.mobs.Player;
 using CastleOfDemise.Scripts.Menus.MultiLauncher;
 
@@ -7,6 +8,8 @@ public partial class IEnnemiesTrackable : Node3D
 {
     
 	[Export] public CharacterBody3D player;
+	[Export] private Node3D _ennemiesNode;
+	
 	
 	public override void _Ready()
 	{
@@ -17,5 +20,16 @@ public partial class IEnnemiesTrackable : Node3D
 	{
 		// GD.Print(player.GlobalTransform.Origin);
 		GetTree().CallGroup("ennemies","UpdateTargetLocation", player.GlobalTransform.Origin);
+		HandleWin();
+		GD.Print(_ennemiesNode.GetChildCount()==0);
+
+	}
+
+	public void HandleWin()
+	{
+		if (_ennemiesNode.GetChildCount()==0)
+		{
+			GetTree().ChangeSceneToFile("res://menus/TitleScreen.tscn");
+		}
 	}
 }
