d4b65133bec985adea146a0ab5304af9ff4ce763
Jean <47366872+jean-voila@users.noreply.github.com>
2024-06-15 14:33:07 +0200

    site astro

--- COD_Docs/astro.config.mjs
+++ COD_Docs/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 	base: 'sqrt100',
 	integrations: [
 		starlight({
-			customCss: ["/sqrt100/src/styles/main.css"],
+			customCss: ["./src/styles/main.css"],
 			title: 'Castle Of Demise',
 			social: {
 				github: 'https://github.com/jean-voila/sqrt100',
