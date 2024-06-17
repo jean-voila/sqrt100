6bb497d7121915d68d520c0715b12fa940cbac98
Jean <47366872+jean-voila@users.noreply.github.com>
2024-06-17 12:19:24 +0200

    FINAL2

--- Castle Of Demise/Scripts/Entities/Player/SFXPlayer.cs
+++ Castle Of Demise/Scripts/Entities/Player/SFXPlayer.cs
@@ -75,6 +75,24 @@ public partial class SFXPlayer : AudioStreamPlayer
 		return 20 * Math.Log10(linear);
 	}
 	
+    
+    
+    
+    
+    
+    
+    
+    /*
+     * Afin de pouvoir gérer uniformément les effets sonores du jeu, nous avons pris le parti d’implémenter, pour chaque nœud pouvant émettre un son, un nœud AudioStreamPlayer lié à une classe SFXPlayer, que nous avons créé.
+       
+       La classe SFXPlayer permet de lier l’objet parent à un fichier audio, de le jouer et de gérer le volume des effets sonores.
+       
+       Cette centralisation des effets sonores permet de simplifier la gestion des sons dans le jeu, en évitant de devoir charger les fichiers audio par chemin absolu à chaque fois que l’on souhaite jouer un son.
+       
+       La méthode PlaySFX permet de jouer un son en fonction de son type, en choisissant aléatoirement un fichier audio parmi ceux disponibles dans le dossier correspondant.
+       La musique est gérée de manière similaire, avec un nœud AudioStreamPlayer dédié à la musique et assignée à son bus audio correspondant.
+
+     */
 	
 	
 	
